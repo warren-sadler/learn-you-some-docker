94a17daf380f48bc2066c9dbfd09e4dc51a96949
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2020-12-02 17:59:55 -0600

    working server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Let's code 🚀
+const express = require("express");
+const app = express();
+
+app.use(require("morgan")("combined"));
+app.get("/", (req, res) => {
+  res.json("Hola 👋");
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
